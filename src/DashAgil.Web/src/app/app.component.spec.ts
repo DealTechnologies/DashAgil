70f95a20a340d08295cc5a6230fda1dd2066c0e3
Everson Jean Humenhuk <everson.humenhuk@ltm.digital>
2020-12-02 17:59:17 -0300

    Adicionado tema ao projeto

--- src/DashAgil.Web/src/app/app.component.spec.ts
+++ src/DashAgil.Web/src/app/app.component.spec.ts
@@ -1,35 +1,33 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'DashAgilWeb'`, () => {
+  it(`should have as title 'angulardark'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('DashAgilWeb');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('angulardark');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('DashAgilWeb app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain(
+      'Welcome to angulardark!'
+    );
   });
 });
--- src/DashAgil.Web/src/app/shared/shared.module.ts
+++ src/DashAgil.Web/src/app/shared/shared.module.ts
@@ -1,19 +1,29 @@
-import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { ComponentsModule } from './components/components.module';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
+import { MaterialModule } from './material.module';
 @NgModule({
-    imports: [
-        FormsModule,
-        ReactiveFormsModule,
-        RouterModule,
-        CommonModule,
-        // MaterialModule,
-        ComponentsModule
-    ],
-    providers: []
+  declarations: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    NgbModule,
+    NgxSpinnerModule
+  ],
+  exports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule,
+    NgbModule,
+    NgxSpinnerModule,
+    MaterialModule
+  ]
 })
-export class SharedModule {
-}
+export class SharedModule {}
