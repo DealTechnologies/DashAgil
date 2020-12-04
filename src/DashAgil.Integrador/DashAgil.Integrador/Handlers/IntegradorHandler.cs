a0d3be9699a81a5d6f24e542858258de2de7ddec
ebasantos <erik_a7x_@hotmail.com>
2020-12-04 19:06:07 -0400

    Adicao de end points de projetos e itens

--- src/DashAgil.Integrador/DashAgil.Integrador/Handlers/IntegradorHandler.cs
+++ src/DashAgil.Integrador/DashAgil.Integrador/Handlers/IntegradorHandler.cs
@@ -2,12 +2,24 @@
 using DashAgil.Integrador.Commands.Output;
 using DashAgil.Integrador.Entidades;
 using DashAgil.Integrador.Infra.Comum;
+using DashAgil.Integrador.Repositorio;
+using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace DashAgil.Integrador.Handlers
 {
-    public class IntegradorHandler : ICommandHandler<SalvarEstoriaCommand>
+    public class IntegradorHandler : ICommandHandler<SalvarEstoriaCommand>,
+                                     ICommandHandler<AtualizarProjetosCommand>,
+                                     ICommandHandler<AtualizarTiposWorkItens>
     {
+        private readonly IAzureDevopsRepository repository;
+
+        public IntegradorHandler(IAzureDevopsRepository repository)
+        {
+            this.repository = repository;
+        }
+
         public async Task<ICommandResult> Handle(SalvarEstoriaCommand command)
         {
             var estoria = new Estoria(command.Nome, command.Descricao);
@@ -18,6 +30,52 @@ namespace DashAgil.Integrador.Handlers
             await Task.CompletedTask;
 
             return new IntegradorCommandResult(true, "sucess", result);
+        }
+
+        public async Task<ICommandResult> Handle(AtualizarProjetosCommand command)
+        {
+            var projetosDevops = await repository.ObterProjetos(command.Organizacao);
+
+            if(projetosDevops is null || !projetosDevops.Value.Any())
+                return new IntegradorCommandResult(false, "falha ao tentar atualizar a base de projetos", null);
+
+            var projetos = projetosDevops.Value.Select(x => new ProjetosDevops
+            {
+                DataUltimaAtualizacao = Convert.ToDateTime(x.LastUpdateTime),
+                Descricao = x.Description,
+                Nome = x.Name,
+                ProjetoId = x.Id
+
+            }).ToList();
+
+            repository.SalvarProjetos(projetos);
+
+            return new IntegradorCommandResult(true, "projetos atualizados com sucesso", projetos);
+        }
+
+        public async Task<ICommandResult> Handle(AtualizarTiposWorkItens command)
+        {
+            var tipos = await repository.ObterWorkItensTypes(command.Organizacao, command.Time, command.Projeto);
+
+            if (tipos is null || !tipos.Value.Any())
+                return new IntegradorCommandResult(false, "falha ao tentar atualizar os tipos de work itens", null);
+
+            var tiposWorkItens = tipos.Value.ToList().Select(x => new TiposWorkItensDevops
+            {
+                Nome = x.Name,
+                Url = x.Url,
+                WorkItemTypeId= x.Id
+
+            }).ToList();
+
+            repository.SalvarTiposWorkItens(tiposWorkItens);
+
+            return new IntegradorCommandResult(true, "Tipos de work itens atualizados com sucesso", tiposWorkItens);
+        }
+
+        public void teste()
+        {
+            repository.test();
         }
     }
 }
