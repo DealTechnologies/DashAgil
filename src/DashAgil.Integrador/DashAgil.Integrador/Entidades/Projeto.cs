0d3377a4a429c5206781734185373fbc21774e07
Hoeliton Schenoveber <hoeliton@outlook.com.br>
2020-12-06 17:53:42 -0300

    ProjetoIntegracao

---
+++ src/DashAgil.Integrador/DashAgil.Integrador/Entidades/Projeto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DashAgil.Integrador.Entidades
+{
+    public class Projeto
+    {
+        public int Id { get; set; }
+        public int? ExternalId { get; set; }
+        public int OrganizacaoId { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public DateTime DataCriacao { get; set; }
+        public DateTime? DataExclusao { get; set; }
+
+    }
---
+++ src/DashAgil.Integrador/DashAgil.Integrador/Entidades/ProjetoIntegracao.cs
@@ -0,0 +1,36 @@
+﻿using DashAgil.Integrador.Jira.Queries;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DashAgil.Integrador.Entidades
+{
+    public class ProjetoIntegracao
+    {
+        public ProjetoIntegracao()
+        {
+
+        }
+
+        public int ProjetoId { get; set; }
+
+        public int ProjetotoIntegracaoId { get; set; }
+
+        public int? ProjetoOrigemId { get; set; }
+
+        public int ProvedorId { get; set; }
+
+        public string UrlOrigem { get; set; }
+
+        public static ProjetoIntegracao PreencherInsercao(BoardQueryResult board)
+        {
+            return new ProjetoIntegracao()
+            {
+                ProjetoId = 1,
+                ProjetoOrigemId = board.Id,
+                ProvedorId = 1,
+                UrlOrigem = board.Url
+            };
+        }
+    }
+}
