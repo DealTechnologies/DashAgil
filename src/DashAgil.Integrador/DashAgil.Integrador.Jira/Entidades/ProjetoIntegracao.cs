2bcbaeaa876eb34be8d40176a18e4387e25782b0
Hoeliton Schenoveber <hoeliton@outlook.com.br>
2020-12-06 17:18:23 -0300

    integracao

---
+++ src/DashAgil.Integrador/DashAgil.Integrador.Jira/Entidades/ProjetoIntegracao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DashAgil.Integrador.Jira.Entidades
+{
+    public class ProjetoIntegracao
+    {
+        public int ProjetoId { get; set; }
+
+        public int ProjetotoIntegracaoId { get; set; }
+
+        public int? ProjetoOrigemId { get; set; }
+
+        public int ProvedorId { get; set; }
+
+        public string UrlOrigem { get; set; }
+    }
+}
