4b58fc980629cccaf3649cdb4709f9a47ea10a8a
Hoeliton Schenoveber <hoeliton@outlook.com.br>
2020-12-06 21:03:49 -0300

    ProjetoIntegracao

---
+++ src/DashAgil.Integrador/DashAgil.Integrador/Repositorio/IProjetoIntegracaoRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using DashAgil.Integrador.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DashAgil.Integrador.Repositorio
+{
+    public interface IProjetoIntegracaoRepositorio
+    {
+        Task Inserir(ProjetoIntegracao projetoIntegracao);
+    }
+}
