ee69cec08d39ca788cef6559452bde307f0055d8
Hoeliton Schenoveber <hoeliton@outlook.com.br>
2020-12-06 21:09:31 -0300

    ProjetoIntegracaoJira

--- src/DashAgil.Integrador/DashAgil.Integrador/Entidades/Projeto.cs
+++ src/DashAgil.Integrador/DashAgil.Integrador/Entidades/Projeto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DashAgil.Integrador.Jira.Queries;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -7,13 +8,25 @@ namespace DashAgil.Integrador.Entidades
     public class Projeto
     {
         public int Id { get; set; }
-        public int? ExternalId { get; set; }
+        public string ExternalId { get; set; }
         public int OrganizacaoId { get; set; }
         public string Nome { get; set; }
         public string Descricao { get; set; }
         public DateTime DataCriacao { get; set; }
         public DateTime? DataModificacao { get; set; }
         public DateTime? DataExclusao { get; set; }
+
+
+
+        public static Projeto PreencherProjetoJira(BoardQueryResult board)
+        {
+            return new Projeto()
+            {
+                ExternalId = Guid.NewGuid().ToString(),
+
+            };
+
+        }
 
     }
 }
--- src/DashAgil.Integrador/DashAgil.Integrador/Entidades/ProjetoIntegracao.cs
+++ src/DashAgil.Integrador/DashAgil.Integrador/Entidades/ProjetoIntegracao.cs
@@ -11,23 +11,24 @@ namespace DashAgil.Integrador.Entidades
         {
 
         }
+
         public int Id { get; set; }
 
-        public int ProjetoId { get; set; }
+        public long ProjetoId { get; set; }
 
         public string ProjetoOrigemId { get; set; } // JIRA OU DEVOPS
 
-        public int ProvedorId { get; set; }
+        public long? ProvedorId { get; set; }
 
         public string UrlOrigem { get; set; }
 
-        public static ProjetoIntegracao PreencherInsercao(BoardQueryResult board)
+        public static ProjetoIntegracao PreencherInsercao(BoardQueryResult board, long projetoId, long? provedorId)
         {
             return new ProjetoIntegracao()
             {
-                ProjetoId = 1,
+                ProjetoId = projetoId,
                 ProjetoOrigemId = board.Id.ToString(),
-                ProvedorId = 1,
+                ProvedorId = provedorId,
                 UrlOrigem = board.Url
             };
         }
