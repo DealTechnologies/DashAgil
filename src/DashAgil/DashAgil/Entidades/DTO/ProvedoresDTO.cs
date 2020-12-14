bbeebda606a0e68c98eaca1dfdb0506aba653056
lsantosh <Dell@DESKTOP-K6NQ7VE>
2020-12-14 02:53:34 -0300

    Login

---
+++ src/DashAgil/DashAgil/Entidades/DTO/ProvedoresDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace DashAgil.Entidades.DTO
+{
+    public class ProvedoresDTO
+    {
+        public int Id { get; set; }
+
+        public string Descricao { get; set; }
+
+        public List<ClientesDTO> Clientes { get; set; }
+    }
+}
---
+++ src/DashAgil/DashAgil/Handlers/AuthHandler.cs
@@ -0,0 +1,39 @@
+﻿using DashAgil.Commands.Input;
+using DashAgil.Commands.Output;
+using DashAgil.Infra.Comum;
+using DashAgil.Repositorio;
+using Flunt.Notifications;
+using System.Threading.Tasks;
+
+namespace DashAgil.Handlers
+{
+    public class AuthHandler : Notifiable, ICommandHandler<AuthCommand>
+    {
+        private readonly IUsuarioRepository usuarioSquadsRepository;
+
+        public AuthHandler(IUsuarioRepository usuarioSquadsRepository)
+        {
+            this.usuarioSquadsRepository = usuarioSquadsRepository;
+        }
+
+        public async Task<ICommandResult> Handle(AuthCommand command)
+        {
+            command.EhValido();
+            AddNotifications(command.Notifications);
+
+            if (Invalid)
+            {
+                new DashAgilCommandResult(false, "auth failed.", Notifications);
+            }
+
+            var usuario = await usuarioSquadsRepository.ObterAsync(command.Email, command.Password);
+            if (usuario == null)
+            {
+                return new DashAgilCommandResult(false, "auth failed.", usuario);
+            }
+
+            var acessos = await usuarioSquadsRepository.ObterAcessosAsync(usuario.Id);
+            return new DashAgilCommandResult(true, "success.", acessos);
+        }
+    }
+}
