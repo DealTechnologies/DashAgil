51bbe61bd41d4d1116913898a683f50b8be884f0
murilo.carrion <murilo.carrion@deal.com.br>
2020-12-20 20:12:55 -0300

    burndown

--- src/DashAgil/DashAgil/Entidades/DashAgil/DemandaHistoricoResult.cs
+++ src/DashAgil/DashAgil/Entidades/DashAgil/DemandaHistoricoResult.cs
@@ -7,5 +7,7 @@ namespace DashAgil.Entidades
         public DateTime Dia { get; set; }
         public int PontosTotalDia { get; set; }
         public int PontosConcluidosDia { get; set; }
+        public double VelocidadeIdeal { get; set; }
+        public double VelocidadeSprint { get; set; }
     }
 }
