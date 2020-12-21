66d3489f64f48533308b0aa488a65c6adc332c7d
Hoeliton Schenoveber <hoeliton@outlook.com.br>
2020-12-21 01:20:38 -0300

    Select Squad

---
+++ src/DashAgil/DashAgil/Queries/DemandaSprintQueryResult.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DashAgil.Queries
+{
+    public class DemandaSprintQueryResult
+    {
+
+    }
+}
+
+
+//d.Id, d.SquadId, d.Tipo, d.Status, d.Descricao, isnull(d.Pontos, 0) as Pontos,
+//                                 s.Nome as SprintNome, s.DataInicio as SprintDataInicio, s.DataFim as SprintDataFim,
+//                                 dh.Id as IdHistorico, dh.DataModificacao, isnull(v.status_novo_num, 1) as StatusDeXPara
